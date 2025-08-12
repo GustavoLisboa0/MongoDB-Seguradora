f7a4297b671985b5c147b7a63e02c8b3fb578fc6
GustavoLisboa0 <gustavo20031010@gmail.com>
2025-08-12 19:49:13 -0300

    Inclusao de arquivos + config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+MONGO_APP_ID = "gustavoLisboa"
+MONGO_API_KEY = "nq38kZS4zuNLCpLU"
+MONGO_CLUSTER = "Cluster0"
+MONGO_DATABASE = "Seguradora_Silvio"
+MONGO_COLLECTION = "Users"
+MONGO_DATA_API_URL = "https://data.mongodb-api.com/app/data-xxxx/endpoint/data/v1/action/insertMany"
